3738acf5ac0ccb34aff96ad87d746a8704d245d6
Ankit Jangir <knjangir24@gmail.com>
2024-07-07 22:33:48 +0530

    updates1234

--- Cabriot-main/js/scripts.js
+++ Cabriot-main/js/scripts.js
@@ -91,3 +91,43 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+<nav class="navbar navbar-expand-lg navbar-light fixed-top shadow-sm" id="mainNav">
+  <div class="container px-5">
+    <a href="/">
+      <img class="navbar-brand" src="./assets/img/cabriot_ventures.svg" alt="" />
+    </a>
+    <button class="navbar-toggler menu" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+      Menu
+      <i class="bi-list"></i>
+    </button>
+    <button class="navbar-toggler menu-keypad" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+      <i class="bi-list"></i>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+      <ul class="navbar-nav ms-auto me-4 my-3 my-lg-0">
+        <li class="nav-item">
+          <a class="nav-link me-lg-3 active" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link me-lg-3" href="eat">Eat</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link me-lg-3" href="stay">Stay</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link me-lg-3" href="go">Go</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link me-lg-3" href="xpress">Xpress</a>
+        </li>
+      </ul>
+      <button class="nav-btn navprimary-btn rounded-pill px-3 mb-2 mb-lg-0" data-bs-toggle="modal" data-bs-target="#feedbackModal">
+        <span class="d-flex align-items-center">
+          <i class="bi-chat-text-fill me-2"></i>
+          <span class="small">Contact</span>
+        </span>
+      </button>
+    </div>
+  </div>
+</nav>
